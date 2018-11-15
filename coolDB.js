8d7c61866bfc2be15578fe43f4e9a9bc37eab1da
Ryan Moore <rymoore@nmu.edu>
2018-11-15 09:42:08 -0500

    DB work

--- coolDB.js
+++ coolDB.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = "";
+var url = ""; //URL for where DB will be located
 
-MongoClient.connect(url, function(err, db){
+MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     CSSConditionRule.log("Database created.");
     db.close();
