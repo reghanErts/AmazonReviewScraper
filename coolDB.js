2337e40b846abb4551a573ddc51130660c0677e4
Ryan Moore <rymoore@nmu.edu>
2018-11-23 14:36:34 -0500

    more db work

--- coolDB.js
+++ coolDB.js
@@ -4,9 +4,14 @@ console.log("Okay so it started");
 MongoClient.connect(url, {useNewUrlParser: true}, function (err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
+    var testObj = {word: "cats", amount: "52"};
     dbo.createCollection("keywords",function(err,db){
         if(err) throw err;
         console.log("collection created.");
+    });
+    dbo.collection("keywords").insertOne(testObj, function(err, res){
+        if(err) throw err;
+        console.log("inserted 1 doc");
     });
     console.log("Database created.");
     db.close();
