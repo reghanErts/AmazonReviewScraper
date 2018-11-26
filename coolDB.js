d41d07217b0a968f2a6dddfeac5e3272777e23ba
Ryan Moore <rymoore@nmu.edu>
2018-11-26 09:19:09 -0500

    Formatting

--- coolDB.js
+++ coolDB.js
@@ -1,16 +1,21 @@
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://localhost:8081/mydb"; //URL for where DB will be located
 console.log("Okay so it started");
-MongoClient.connect(url, {useNewUrlParser: true}, function (err, db) {
+MongoClient.connect(url, {
+    useNewUrlParser: true
+}, function (err, db) {
     if (err) throw err;
     var dbo = db.db("mydb");
-    var testObj = {word: "cats", amount: "52"};
-    dbo.createCollection("keywords",function(err,db){
-        if(err) throw err;
+    var testObj = {
+        word: "cats",
+        amount: "52"
+    };
+    dbo.createCollection("keywords", function (err, db) {
+        if (err) throw err;
         console.log("collection created.");
     });
-    dbo.collection("keywords").insertOne(testObj, function(err, res){
-        if(err) throw err;
+    dbo.collection("keywords").insertOne(testObj, function (err, res) { //Use insertMany
+        if (err) throw err;
         console.log("inserted 1 doc");
     });
     console.log("Database created.");
