02985f29dbb0949ce6ae00b7f848cdf389ddfc45
Cole <cbornema@nmu.edu>
2018-12-06 20:27:44 -0500

    products review.

--- pub/assets/js/client.js
+++ pub/assets/js/client.js
@@ -1,5 +1,4 @@
 // Cole Bornemann
-
 var socket = io();
 
 // Clear Error field. and search field.
@@ -7,6 +6,21 @@ function clearSearch() {
     $("#searchError").html("");
     $("#searchText").val("");
     $("#SearchText").trigger("blur");
+}
+
+function showResults() {
+    $("#Product").hide();
+    $("#searchResults").show();
+}
+
+function showProduct() {
+    $("#searchResults").hide();
+    $("#Product").show();
+}
+
+// Overwrite print for easier console.log()
+function print(data) {
+    console.log(data);
 }
 
 // The stuff enetered matched several products, and should be narrowed (to reduce server load etc.)
@@ -17,13 +31,24 @@ socket.on("productList", function (dataFromServer) {
 
 // The word (or words) was narrow enough to be matched to one product.
 socket.on("reviews", function (dataFromServer) {
-    $('#searchResults').hide();
     clearSearch();
-    console.log("Reviews.");
-    console.log();
-    for (let i = 0; i < dataFromServer.reviews.length; i++) {
-
+    // Only clear product reviews when new ones are coming in.
+    $("#ProductReviews").html("");
+    print(dataFromServer);
+    // H2
+    $("#ProductName").html(dataFromServer.name);
+    // Review Ratings
+    let a = "<thead><tr><th>5 Star</th><th>4 Star</th><th>3 Star</th><th>2 Star</th><th>1 Star</th></tr></thead><tbody><tr>";
+    for (let key in dataFromServer.ratings) {
+        a += "<td>" + dataFromServer.ratings[key] + "</td>";
     }
+    a += "</tr></tbody>";
+    $("#ProductRatings").html(a);
+    // Review text
+    for (let i = 0; i < dataFromServer.reviews.length; i++) {
+        $("#ProductReviews").append(dataFromServer.reviews[i].review_text + "<br><br>");
+    }
+    showProduct();
 });
 
 // Server couldn't find any products containing the word (or words).
@@ -42,7 +67,7 @@ function updateTableChoose(dataFromServer) {
         $("#searchResults").append(tableText);
     }
     $("#searchResults").append("</tbody>");
-    $("#searchResults").show();
+    showResults();
 }
 
 function setUpEventHandlers() {
@@ -51,10 +76,11 @@ function setUpEventHandlers() {
         socket.emit("findItem", $("#searchText").val());
     });
 
-    $("#bgc").on('click', 'table tr td', function() {
+    $("#bgc").on('click', 'table tr td', function () {
         // https://stackoverflow.com/questions/13514878/jquery-click-handler-not-working-in-a-table
         socket.emit("findItem", $(this).html())
     });
+    showResults();
 
     // Makes the body hidden until everything is loaded.
     $("body").prop("hidden", false);
